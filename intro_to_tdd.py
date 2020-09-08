5d15957f8d50bbe373419f62f9626c230cc69b3c
Stephen Graham <sgraham42@gmail.com>
2020-09-08 11:53:12 -0700

    done with non-bonus intro to tdd

--- intro_to_tdd.py
+++ intro_to_tdd.py
@@ -38,20 +38,29 @@ class IsPalindromeTests(unittest.TestCase):
     def testFive(self):
         self.assertNotEqual(isPalindrome('racecar'), False)
 
-# def coins(amount):
-#     pass
 
-# class IsPalindromeTests(unittest.TestCase):
-#     def testOne(self):
-#         self.assertEqual(coins(87), [3, 1, 0, 2])
-    # def testTwo(self):
-    #     self.assertEqual(isPalindrome('kayak'), True)
-    # def testThree(self):
-    #     self.assertTrue(isPalindrome('step on no pets'))
-    # def testFour(self):
-    #     self.assertFalse(isPalindrome('step on on pets'))
-    # def testFive(self):
-    #     self.assertNotEqual(isPalindrome('racecar'), False)
+def coins(amount):
+    coinsArr = [0, 0, 0, 0]
+    coinsArr[0] = int(amount/25)
+    amount -= coinsArr[0] * 25
+    coinsArr[1] = int(amount/10)
+    amount -= coinsArr[1] * 10
+    coinsArr[2] = int(amount/5)
+    amount -= coinsArr[2] * 5
+    coinsArr[3] = amount
+    return coinsArr
+
+class coinsTests(unittest.TestCase):
+    def testOne(self):
+        self.assertEqual(coins(87), [3, 1, 0, 2])
+    def testTwo(self):
+        self.assertEqual(coins(100), [4, 0, 0, 0])
+    def testThree(self):
+        self.assertEqual(coins(47), [1, 2, 0, 2])
+    def testFour(self):
+        self.assertEqual(coins(99), [3, 2, 0, 4])
+    def testFive(self):
+        self.assertEqual(coins(7), [0, 0, 1, 2])
 
 
 if __name__ == '__main__':
